2e6ef04594303729445d3ad56b7a738a0e5e5711
Tony Allevato <allevato@google.com>
2024-08-21 12:01:31 -0700

    Internal change.
    PiperOrigin-RevId: 665974712

--- example/catalog/Catalog/AppDelegate.swift
+++ example/catalog/Catalog/AppDelegate.swift
@@ -14,15 +14,18 @@
  limitations under the License.
  */
 
-import UIKit
 import CatalogByConvention
+import UIKit
 
-@UIApplicationMain
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+  ) -> Bool {
     self.window = UIWindow(frame: UIScreen.main.bounds)
 
     let rootViewController = CBCNodeListViewController(node: CBCCreateNavigationTree())
